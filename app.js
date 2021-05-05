500d12596abf66e1da1d6d3f837a858ae9bf86e2
Hrithik Diwakar <hrithikdiwakar13@gmail.com>
2021-05-05 20:17:51 +0530

    Revert "Revert "final app""
    This reverts commit 4c99f067a0f384b71b4b23a1c96fbf948c54daa3.

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-var btnTranslate = document.querySelectorAll("#btn-translate");
+var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
-var txtOutput = document.querySelector("#txt-output");
+var outputDiv = document.querySelector("#output");
+
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
 var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
-function getTranslationURL(input){
+//
+function getTranslationURL(input) {
     return serverURL + "?" + "text=" + input
 }
 
@@ -13,15 +16,18 @@ function errorHandler(error) {
     alert("something wrong with server! try again after some time")
 }
 
-function clickHandler(){
-    var  inputText = txtInput.value;
-    console.log("insside function")
-    fetch(getTranslationURL(inputText)).then(response => response.json()).then(json => {
-        var translatedtext = json.contents.translated;
-        txtOutput.innerText = translatedtext;
-    })
-    .catch(errorHandler)
+
+function clickHandler() {
+    var inputText = txtInput.value; // taking input
+
+    // calling server for processing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText; // output
+           })
+        .catch(errorHandler)
 };
 
-
-btnTranslate.addEventListener("click", clickHandler);
+btnTranslate.addEventListener("click", clickHandler)
